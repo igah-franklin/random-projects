1673e1469f8fc6ac78e4d43e064843ec6a942725
igah-franklin <igahfranklyn@gmail.com>
2022-06-12 13:18:51 +0100

    constructor objects

--- oop/constructorFunction/challenge.js
+++ oop/constructorFunction/challenge.js
@@ -9,14 +9,16 @@ const car1 = new Car("BMW", 120);
 const car2 = new Car("Mercedes", 95);
 
 Car.prototype.accelerate = function () {
-  return this.speed * 10;
+  return (this.speed += 10);
 };
 
 Car.prototype.brake = function () {
-  return this.speed - 5;
+  return (this.speed -= 5);
 };
 
-console.log("increase in speed:", car1.accelerate());
-console.log("new speed:", car2.accelerate());
-console.log("brake car1", car1.brake());
-console.log("brake car2", car2.brake());
+console.log("increase in speed:", car1.accelerate(), "km/hr");
+console.log("increase in speed:", car1.accelerate(), "km/hr");
+console.log("new speed:", car2.accelerate(), "km/hr");
+
+console.log("brake car1", car1.brake(), "km/hr");
+console.log("brake car2", car2.brake(), "km/hr");
